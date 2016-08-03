f5202e17f586e00da36673742e836054d8bb7862
Patrik Nilsson <pn222en@student.lnu.se>
2016-08-03 21:34:41 +0200

    add header and image form

--- configure/configure.js
+++ configure/configure.js
@@ -2,10 +2,15 @@ var express = require("express");
 var app = express();
 var exphbs  = require('express-handlebars');
 var path = require("path");
+var bodyParser = require("body-parser");
 
 module.exports = function () {
 
   app.use(express.static(path.join(__dirname, "../public")));
+
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+
 
   app.engine('handlebars', exphbs.create({
      layoutsDir: app.get("views") + "/layouts",
