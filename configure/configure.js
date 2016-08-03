eaeb913bbfeeda0c395e6dc19757fbf97b0df9c0
Patrik Nilsson <pn222en@student.lnu.se>
2016-08-03 18:19:57 +0200

    add partials and public

--- configure/configure.js
+++ configure/configure.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var app = express();
 var exphbs  = require('express-handlebars');
+var path = require("path");
 
 module.exports = function () {
+
+  app.use(express.static(path.join(__dirname, "../public")));
+
   app.engine('handlebars', exphbs.create({
      layoutsDir: app.get("views") + "/layouts",
      defaultLayout: "main",
+     partialsDir: app.get("views") + "/partials",
+
   }).engine);
   
   app.set('view engine', 'handlebars');
